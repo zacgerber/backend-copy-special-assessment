63253c2fc543bab819c07be2dc6c2f787b625be2
zac Gerber <Zgerber91@gmail.com>
2020-06-28 12:46:06 -0400

    working code need to add a doc string

--- copyspecial.py
+++ copyspecial.py
@@ -19,18 +19,32 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    # your code here
-    return
+    paths = os.listdir(dirname)
+
+    special_paths = []
+    for filename in paths:
+        match = re.search(r'__\w+__', filename)
+        if match:
+            special_paths.append(os.path.abspath(
+                os.path.join(dirname, filename)))
+
+    return special_paths
 
 
 def copy_to(path_list, dest_dir):
-    # your code here
+    if not os.path.isdir(dest_dir):
+        os.makedirs(dest_dir)
+    for path in path_list:
+        shutil.copy(path, dest_dir)
     return
 
 
 def zip_to(path_list, dest_zip):
-    # your code here
-    return
+    cmd = ['zip', '-j', dest_zip]
+    cmd.extend(path_list)
+    print("Command I'm going to do:")
+    print(' '.join(cmd))
+    subprocess.check_output(cmd)
 
 
 def main(args):
@@ -39,7 +53,7 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    # TODO: add one more argument definition to parse the 'from_dir' argument
+    parser.add_argument('from_dir', help='dest zipfile for special files')
     ns = parser.parse_args(args)
 
     # TODO: you must write your own code to get the command line args.
@@ -49,8 +63,15 @@ def main(args):
     # This is input data validation. If something is wrong (or missing) with
     # any required args, the general rule is to print a usage message and
     # exit(1).
-
-    # Your code here: Invoke (call) your functions
+    # if len(sys.argv) > 1:
+    #     parser.print_usage()
+    special_paths = get_special_paths(ns.from_dir)
+    if ns.todir:
+        copy_to(special_paths, ns.todir)
+    elif ns.tozip:
+        zip_to(special_paths, ns.tozip)
+    else:
+        print('\n'.join(special_paths))
 
 
 if __name__ == "__main__":
